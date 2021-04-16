400b711070774dee0216124c57176edda76d9760
Lucas Nascimento <lucasnascimentoribeiro13@gmail.com>
2021-04-15 21:50:42 -0300

    Read files and use its data

--- src/CSIManager.cpp
+++ src/CSIManager.cpp
@@ -1,5 +1,9 @@
+#include <string.h>
+
+#include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "./lib/Color.h"
 #include "./model/DNADatabase.h"
@@ -8,27 +12,101 @@
 using namespace std;
 using namespace Color;
 
-#define ALICE "AGACGGGTTACCATGACTATCTATCTATCTATCTATCTATCTATCTATCACGTACGTACGTATCGAGATAGATAGATAGATAGATCCTCGACTTCGATCGCAATGAATGCCAATAGACAAAA"
-#define BOB "AACCCTGCGCGCGCGCGATCTATCTATCTATCTATCCAGCATTAGCTAGCATCAAGATAGATAGATGAATTTCGAAATGAATGAATGAATGAATGAATGAATG"
-#define CHARLIE "CCAGATAGATAGATAGATAGATAGATGTCACAGGGATGCTGAGGGCTGCTTCGTACGTACTCCTGATTTCGGGGATCGCTGACACTAATGCGTGCGAGCGGATCGATCTCTATCTATCTATCTATCTATCCTATAGCATAGACATCCAGATAGATAGATC"
-#define NO_MATCH "GGTACAGATGCAAAGATAGATAGATGTCGTCGAGCAATCGTTTCGATAATGAATGAATGAATGAATGAATGAATGACACACGTCGATGCTAGCGGCGGATCGTATATCTATCTATCTATCTATCAACCCCTAG"
+vector<string> split(string& str) {
+  vector<string> substrings;
 
-int main() {
+  string token;
+  size_t position{0};
+  string delimiter{","};
+
+  while ((position = str.find(delimiter)) != string::npos) {
+    token = str.substr(0, position);
+    substrings.push_back(token);
+    str.erase(0, position + delimiter.length());
+  }
+  token = str.substr(0, position);
+  substrings.push_back(token);
+
+  return substrings;
+}
+
+int main(int argc, char** argv) {
+  bool has_database{false}, has_sequence{false};
+  for (int i = 0; i < argc; i++)
+    if (strcmp(argv[i], "-d") == 0)
+      has_database = true;
+    else if (strcmp(argv[i], "-s") == 0)
+      has_sequence = true;
+
+  if (!has_database) {
+    cout << tcolor("The database filename was not provided. Use the flag -d to provide it", RED);
+    return 1;
+  }
+
+  if (!has_sequence) {
+    cout << tcolor("The dna sequence filename was not provided. Use the flag -s to provide it", RED);
+    return 1;
+  }
+
+  if (argc < 5) {
+    cout << tcolor("Invalid arguments", RED);
+    return 1;
+  }
+
+  string database_filename, dna_sequence_filename;
+  for (int i{1}; i < argc; i++)
+    if (i % 2 != 0)
+      if (strcmp(argv[i], "-d") == 0)
+        database_filename = argv[i + 1];
+      else if (strcmp(argv[i], "-s") == 0)
+        dna_sequence_filename = argv[i + 1];
+
+  ifstream database_file(database_filename);
+  if (!database_file.good()) {
+    cout << tcolor("No database file was found with the provided filename", RED) << endl;
+    database_file.close();
+    return 1;
+  }
+
+  ifstream dna_sequence_file(dna_sequence_filename);
+  if (!dna_sequence_file.good()) {
+    cout << tcolor("No dna sequence file was found with the provided filename", RED) << endl;
+    dna_sequence_file.close();
+    return 1;
+  }
+
   DNADatabase database;
-  database.add_item("5 2 8", "Alice");
-  database.add_item("3 7 4", "Bob");
-  database.add_item("6 1 5", "Charlie");
+  string name;
+  stringstream key;
+  vector<string> substrings;
+
+  string line;
+  getline(database_file, line);
+  while (getline(database_file, line)) {
+    substrings = split(line);
+
+    key << substrings[1] << " " << substrings[2] << " " << substrings[3];
+    name = substrings[0];
+
+    database.add_item(key.str(), name);
+    key.str(string());
+  }
+  database_file.close();
+
+  string dna_sequence;
+  getline(dna_sequence_file, dna_sequence);
+  dna_sequence_file.close();
 
   Profiler profiler;
-  auto key = profiler.generate_profile(CHARLIE);
-  auto name = database.get_dictionary()[key];
+  auto profile = profiler.generate_profile(dna_sequence);
+  auto found = database.get_dictionary()[profile];
 
-  if (name.length() == 0) {
+  if (found.length() == 0) {
     cout << tcolor("No match", RED) << endl;
     return 1;
   }
 
-  cout << tcolor("The subject is: ", BLUE) << tcolor(name, WHITE, BOLD) << endl;
+  cout << tcolor("The subject is: ", BLUE) << tcolor(found, WHITE, BOLD) << endl;
 
   return 0;
 }
